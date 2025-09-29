1bbb6a1fc497bac1665f1898db3b1a7ed91f6a2a
Sincesix-pdf <faizalyushrilimansyah@gmail.com>
2025-09-29 23:32:31 +0700

    fix styling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",       // aktifin static export
+  images: { unoptimized: true }, // biar next/image jalan di export
 };
 
 export default nextConfig;
